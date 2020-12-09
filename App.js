827da23fc13dc9ce08e9863255b09f36d4cbb866
Isaac <isaacgc08@gmail.com>
2020-12-09 04:05:18 -0800

    [New] Installed ESLint: Did not fixed prettier's sytenx to eslint

--- App.js
+++ App.js
@@ -1,25 +1,13 @@
 import React from 'react';
-import { StyleSheet, ScrollView } from 'react-native';
-import Start from './components/Start';
-import Chat from './components/Chat';
-import 'react-native-gesture-handler';
+import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import 'react-native-gesture-handler';
+
+import Start from './components/Start';
+import Chat from './components/Chat';
 
 const Stack = createStackNavigator();
-
-export default class App extends React.Component {
-  render() {
-    return (
-      <NavigationContainer style={styles.container}>
-        <Stack.Navigator initialRouteName="Start">
-          <Stack.Screen name="Start" component={Start} />
-          <Stack.Screen name="Chat" component={Chat} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    );
-  }
-}
 
 const styles = StyleSheet.create({
   container: {
@@ -32,3 +20,14 @@ const styles = StyleSheet.create({
     flex: 1,
   },
 });
+
+export default function App() {
+  return (
+    <NavigationContainer style={styles.container}>
+      <Stack.Navigator initialRouteName='Start'>
+        <Stack.Screen name='Start' component={Start} />
+        <Stack.Screen name='Chat' component={Chat} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
